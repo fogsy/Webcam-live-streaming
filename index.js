473f03a0dc816567bd63150d9d4b7f3fbe300d1f
Ranit Dey <you@example.com>
2019-03-21 13:21:12 +0530

    added basic live streaming fro webcam

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const path = require("path")
 const express = require("express")
 const app= express()
+const server = app.listen(process.env.NODE_PORT||3000)
+const io= require("socket.io").listen(server)
+const cv = require("opencv4nodejs")
 
-
+const FPS=30
+const videoCapture =new cv.VideoCapture(0)
+videoCapture.set(cv.CAP_PROP_FRAME_HEIGHT,300)
+videoCapture.set(cv.CAP_PROP_FRAME_WIDTH,300)
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"index.html"))
 
 })
+setInterval(()=>{
+    const frame= videoCapture.read()
+    const image=cv.imencode(".jpg",frame).toString("base64")
+    io.emit('image',image)
 
-app.listen(3000,()=>{
-    console.log("listening on 3000")
-})
+ 
+},1000/FPS)
+
+
