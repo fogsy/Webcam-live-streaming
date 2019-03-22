46293d6f2c48f936860195ee89aed138a8471904
Ranit Dey <you@example.com>
2019-03-22 11:19:15 +0530

    resolved a bug that it was not working in remote machine

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const express = require("express")
 const app= express()
-const server = app.listen(process.env.NODE_PORT||3000)
+const server = app.listen(3000,"0.0.0.0")
 const io= require("socket.io").listen(server)
 const cv = require("opencv4nodejs")
 
@@ -11,12 +11,17 @@ videoCapture.set(cv.CAP_PROP_FRAME_HEIGHT,300)
 videoCapture.set(cv.CAP_PROP_FRAME_WIDTH,300)
 
 app.get("/",(req,res)=>{
+    res. header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
     res.sendFile(path.join(__dirname,"index.html"))
 
 })
 setInterval(()=>{
     const frame= videoCapture.read()
     const image=cv.imencode(".jpg",frame).toString("base64")
+  
     io.emit('image',image)
 
  
